955b00869967ba694fa6603fc857865b765e7cfc
NguyenNguyen <nguyenbk92@gmail.com>
2019-12-03 12:02:33 +0700

    Update bin directory

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 export GO111MODULE=off
-rm -rf build
+rm -rf bin
 go get -v
 for GOOS in darwin linux windows; do
     for GOARCH in 386 amd64; do
-        go build -v -o build/tomox-bot-$GOOS-$GOARCH
+        go build -v -o bin/tomox-bot-$GOOS-$GOARCH
     done
 done
 
