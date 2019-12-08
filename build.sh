57ee31711d8f1b7b7058c32ffb739305e4ef545f
NguyenNguyen <nguyenbk92@gmail.com>
2019-12-08 11:09:42 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -rf bin
 go get -v
 for GOOS in darwin linux windows; do
     for GOARCH in 386 amd64; do
-        go build -v -o bin/tomox-bot-$GOOS-$GOARCH
+        env GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=1 go build -v -o bin/tomox-bot-$GOOS-$GOARCH
     done
 done
 
