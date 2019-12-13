075a4e550b16d0d485dd35e365edfb613c4a2fcb
NguyenNguyen <nguyenbk92@gmail.com>
2019-12-13 14:03:50 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ go get -v github.com/karalabe/xgo # Go CGO cross compiler
 go build $GOPATH/src/github.com/karalabe/xgo/xgo.go
 
 
-$GOPATH/src/github.com/karalabe/xgo/xgo -targets="linux/amd64,linux/386,darwin/amd64,darwin/386,windows/amd64,windows/386" -out bin/tomox-bot-v0.7 github.com/thanhnguyennguyen/tomox-bot
+./xgo -go 1.12.5 -targets="linux/amd64,linux/386,darwin/amd64,darwin/386,windows/amd64,windows/386" -out bin/tomox-bot-v0.7 github.com/thanhnguyennguyen/tomox-bot
 
